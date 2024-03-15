48bcc3b69bdea783ed87e9bf83be05d69ff96f1d
brunovt074 <brunodev.pdl@gmail.com>
2024-03-15 00:57:48 -0300

    Created factories

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -16,8 +16,15 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
+        $status = $this->faker->randomElement('B','P','V');
+
         return [
-            //
+            
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_dated' => $this->faker->dateTimeThisDecade(),
+            'paid_dated' => $this->faker->dateTimeThisDecade()
         ];
     }
 }

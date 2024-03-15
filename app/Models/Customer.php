8875e9bec04823a7f966f88b05d0d2d6c75e778f
brunovt074 <brunodev.pdl@gmail.com>
2024-03-15 00:48:36 -0300

    Create relationships between Customer and Invoice

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+    ];
+
+    public function invoices(): HasMany{
+        return $this->hasMany(Invoice::class);
+    }
 }
+
+
--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+
+    ];
+
+    public function customer(){
+
+        return $this->belongsTo(Customer::class);
+
+    }
 }
